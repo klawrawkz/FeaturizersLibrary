4a192110f35d45e287bbd002866ca3999557180c
wangyems <wangye@microsoft.com>
2020-01-19 22:42:21 -0800

    update

--- src/Strings.h
+++ src/Strings.h
@@ -13,14 +13,14 @@ namespace Featurizer {
 template <typename IteratorT>
 inline std::string ToLower(IteratorT begin, IteratorT end) {
     std::string result(begin, end);
-    std::transform(result.begin(), result.end(), result.begin(), [](char c) { return std::tolower(c); });
+    std::transform(result.begin(), result.end(), result.begin(), ::tolower);
     return result;
 }
 
 template <typename IteratorT>
 inline std::string ToUpper(IteratorT begin, IteratorT end) {
     std::string result(begin, end);
-    std::transform(result.begin(), result.end(), result.begin(), [](char c) { return std::toupper(c); });
+    std::transform(result.begin(), result.end(), result.begin(), ::toupper);
     return result;
 }
 
@@ -37,26 +37,20 @@ inline std::string TrimLeft(IteratorT begin, IteratorT end, PredicateT isPredica
 
 template <typename IteratorT, typename PredicateT>
 inline std::string TrimRight(IteratorT begin, IteratorT end, PredicateT isPredicate) {
-    while (begin != end) {
-        if (*end == isPredicate)
-            --end;
-        else
-            break;
+    IteratorT offset = begin;
+    IteratorT iter = begin;
+    while (iter != end) {
+        if (*iter != isPredicate)
+            offset = iter;
+        ++iter;
     }
-    return std::string(begin, end);
+    return std::string(begin, offset + 1);
 }
 
 template <typename IteratorT, typename PredicateT>
 inline std::string Trim(IteratorT begin, IteratorT end, PredicateT isPredicate) {
-    while (begin != end) {
-        if (*begin == isPredicate)
-            ++begin;
-        else if (*end == isPredicate)
-            --end;
-        else
-            break;
-    }
-    return std::string(begin, end);
+    std::string stringAfterTrimedLeft(TrimLeft(begin, end, isPredicate));
+    return TrimRight(stringAfterTrimedLeft.begin(), stringAfterTrimedLeft.end(), isPredicate);
 }
 
 template <typename IteratorT, typename IsDelimiterT>
@@ -82,23 +76,96 @@ inline void Parse(std::string const &input, IsDelimiterT const &isDelimiter, std
         callback(left, right);
 }
 
+template <typename IteratorT, typename RegexT>
+inline void ParseRegex(std::string const &input, RegexT const &regexToken, std::function<void (IteratorT, IteratorT)> const &callback) {
+    std::sregex_iterator iter(input.begin(), input.end(), regexToken);
+    std::sregex_iterator end;
 
-
-// //ngram
-// //question: tuple<IteratorT*, size_t> or tuple<IteratorT*, IteratorT*>
-// template<typename IteratorT, typename RangeT>
-// inline std::vector<std::tuple<IteratorT, IteratorT>> ngram_string(const IteratorT& inputbegin, const IteratorT& inputend, RangeT ngram_range);
-// template<typename IteratorT, typename RangeT>
-// inline std::vector<std::tuple<IteratorT, IteratorT>> ngram_string(IteratorT& inputbegin, IteratorT& inputend, RangeT ngram_range);
-// template<typename IteratorT, typename RangeT>
-// inline std::vector<std::tuple<IteratorT, IteratorT>> ngram_char(const IteratorT& inputbegin, const IteratorT& inputend, RangeT ngram_range);
-// template<typename IteratorT, typename RangeT>
-// inline std::vector<std::tuple<IteratorT, IteratorT>> ngram_char(IteratorT& inputbegin, IteratorT& inputend, RangeT ngram_range);
-// template<typename IteratorT, typename RangeT>
-// inline std::vector<std::tuple<IteratorT, IteratorT>> ngram_charwb(const IteratorT& inputbegin, const IteratorT& inputend, RangeT ngram_range);
-// template<typename IteratorT, typename RangeT>
-// inline std::vector<std::tuple<IteratorT, IteratorT>> ngram_charwb(IteratorT& inputbegin, IteratorT& inputend, RangeT ngram_range);
-
+    while (iter != end) {
+        std::string const iterStringConst = iter->str();
+        callback(iterStringConst.begin(), iterStringConst.end());
+        ++iter;
+    }
+}
 
 } // namespace Featurizer
 } // namespace Microsoft
+
+
+// inline void Trim(std::string& input) {
+//     //replace all punctuations with spaces
+//     std::transform(input.begin(), input.end(), input.begin(), [](char c) {
+//         if (std::ispunct(c) )
+//             return ' ';
+//         return c;
+//     });
+//     //remove duplicate whitespace
+//     bool prev_is_space = true;
+//     input.erase(std::remove_if(input.begin(), input.end(), [&prev_is_space](unsigned char curr) {
+//         bool r = std::isspace(curr) && prev_is_space;
+//         prev_is_space = std::isspace(curr);
+//         return r;
+//     }), input.end());
+// }
+// #if (defined _MSC_VER)
+// #   pragma warning(pop)
+// #endif
+// inline void RemoveWhiteSpace(std::string& input) {
+//     input.erase(std::remove_if(input.begin(), input.end(), ::isspace), input.end());
+// }
+
+// inline std::vector<std::string> NgramParsingWord(std::string const &input, std::uint8_t ngram_range_min, std::uint8_t ngram_range_max) {
+//     if (ngram_range_min < 1 || ngram_range_max < ngram_range_min)
+//         throw std::invalid_argument("invalid ngram range");
+//     std::vector<std::string> ngramDocument;
+//     std::vector<size_t> spaceIdx;
+//     spaceIdx.emplace_back(0);
+//     size_t idx = 0;
+//     for (auto it = input.begin(); it != input.end(); ++it, ++idx) {
+//         if (std::isspace(*it))
+//             spaceIdx.emplace_back(idx + 1);
+//     }
+
+//     //need optimize
+//     for (auto ngramVal = ngram_range_min; ngramVal <= ngram_range_max; ++ngramVal) {
+//         for (size_t wordIdx = 0; wordIdx < spaceIdx.size() - ngramVal; ++wordIdx) {
+//             ngramDocument.emplace_back(input.substr(spaceIdx[wordIdx], spaceIdx[wordIdx + ngramVal] - spaceIdx[wordIdx] - 1));
+//         }
+//     }
+//     return ngramDocument;
+// }
+// inline std::vector<std::string> NgramParsingChar(std::string const &input, std::uint8_t ngram_range_min, std::uint8_t ngram_range_max) {
+//     if (ngram_range_min < 1 || ngram_range_max < ngram_range_min)
+//         throw std::invalid_argument("invalid ngram range");
+//     std::vector<std::string> ngramDocument;
+//     for (auto ngramVal = ngram_range_min; ngramVal <= ngram_range_max; ++ngramVal) {
+//         for (size_t segIdx = 0; segIdx < input.size() - ngramVal + 1; ++segIdx) {
+//             ngramDocument.emplace_back(input.substr(segIdx, ngramVal));
+//         }
+//     }
+//     if (ngramDocument.size() == 0)
+//         std::runtime_error("empty vocabulary");
+//     return ngramDocument;
+// }
+// inline std::vector<std::string> NgramParsingCharwb(std::vector<std::string> const &input, std::uint8_t ngram_range_min, std::uint8_t ngram_range_max) {
+//     if (ngram_range_min < 1 || ngram_range_max < ngram_range_min)
+//         throw std::invalid_argument("invalid ngram range");
+//     std::vector<std::string> ngramDocument;
+//     for (std::string word : input) {
+//         std::string paddingWord = " " + word + " ";
+//         size_t paddingWordSize = paddingWord.size();
+//         for (auto ngramVal = ngram_range_min; ngramVal <= ngram_range_max; ++ngramVal) {
+//             if (ngramVal >= paddingWordSize) {
+//                 ngramDocument.emplace_back(paddingWord);
+//                 break;
+//             }
+//             for (size_t segIdx = 0; segIdx < paddingWordSize - ngramVal + 1; ++segIdx) {
+//                 ngramDocument.emplace_back(paddingWord.substr(segIdx, ngramVal));
+//             }
+
+//         }
+//     }
+//     if (ngramDocument.size() == 0)
+//         std::runtime_error("empty vocabulary");
+//     return ngramDocument;
+// }
--- src/UnitTests/Strings_UnitTest.cpp
+++ src/UnitTests/Strings_UnitTest.cpp
@@ -9,3 +9,131 @@
 
 using namespace Microsoft::Featurizer;
 
+void ParseTest(std::string const & input, std::vector<std::string> const & label) {
+    std::vector<std::string> output;
+    Parse<std::string::const_iterator, char>(
+        input,
+        ' ',
+        [&output] (std::string::const_iterator & iter_start, std::string::const_iterator & iter_end) {
+            output.emplace_back(std::string(iter_start, iter_end));
+        }
+    );
+    CHECK(output == label);
+}
+
+void ParseRegexTest(std::string const & input, std::vector<std::string> const & label, std::string const & token = "[^\\s]+") {
+    std::vector<std::string> output;
+    std::regex rgx(token);
+    ParseRegex<std::string::const_iterator, std::regex>(
+        input,
+        rgx,
+        [&output] (std::string::const_iterator iter_start, std::string::const_iterator iter_end) {
+            output.emplace_back(std::string(iter_start, iter_end));
+        }
+    );
+    CHECK(output == label);
+}
+
+TEST_CASE("ToLower") {
+    std::string input("THIS IS THE FIRST DOCUMENT.");
+    std::string label("this is the first document.");
+    std::string output(ToLower(input.begin(), input.end()));
+    CHECK(output == label);
+}
+
+TEST_CASE("ToUpper") {
+    std::string input("this is the first document.");
+    std::string label("THIS IS THE FIRST DOCUMENT.");
+    std::string output(ToUpper(input.begin(), input.end()));
+    CHECK(output == label);
+}
+
+TEST_CASE("TrimLeft") {
+    std::string input("    this is the first document.");
+    std::string label("this is the first document.");
+    std::string output(TrimLeft(input.begin(), input.end(), ' '));
+    CHECK(output == label);
+}
+
+TEST_CASE("TrimRight") {
+    std::string input("this is the first document.        ");
+    std::string label("this is the first document.");
+    std::string output(TrimRight(input.begin(), input.end(), ' '));
+    CHECK(output == label);
+}
+
+TEST_CASE("Trim") {
+    std::string input("     this is the first document.     ");
+    std::string label("this is the first document.");
+    std::string output(Trim(input.begin(), input.end(), ' '));
+    CHECK(output == label);
+}
+
+TEST_CASE("Parse") {
+    ParseTest("this is a document", {"this", "is", "a", "document"});
+    ParseTest("this   is a   document ", {"this", "is", "a", "document"});
+    ParseTest(" this is   a document", {"this", "is", "a", "document"});
+    ParseTest(" this is a document ", {"this", "is", "a", "document"});
+    ParseTest(" this   is a   document  ", {"this", "is", "a", "document"});
+    ParseTest("this", {"this"});
+    ParseTest(" this", {"this"});
+    ParseTest("this ", {"this"});
+    ParseTest(" this ", {"this"});
+    ParseTest("  this  ", {"this"});
+    ParseTest("", {});
+    ParseTest(" ", {});
+    ParseTest("  ", {});
+}
+
+TEST_CASE("ParseRegex") {
+    ParseRegexTest("this is a document", {"this", "is", "a", "document"});
+    ParseRegexTest("this   is a   document ", {"this", "is", "a", "document"});
+    ParseRegexTest(" this is   a document", {"this", "is", "a", "document"});
+    ParseRegexTest(" this is a document ", {"this", "is", "a", "document"});
+    ParseRegexTest(" this   is a   document  ", {"this", "is", "a", "document"});
+}
+
+
+// TEST_CASE("TextPreprocessorTransformer_ngramsword") {
+//     std::vector<std::string> stopwords({});
+//     NS::Featurizers::Components::TextPreprocessorTransformer tp(true, "\\s+", stopwords, "ascii", 1, 2, "word");
+//     std::string input(" Bi-grams    are cool! ");
+//     std::vector<std::string> label({"bi", "grams", "are", "cool", "bi grams", "grams are", "are cool"});
+//     std::vector<std::string> out = tp.execute(input);
+//     CHECK(out == label);
+// }
+// TEST_CASE("TextPreprocessorTransformer_ngramschar") {
+//     std::vector<std::string> stopwords({});
+//     NS::Featurizers::Components::TextPreprocessorTransformer tp(true, "\\s+", stopwords, "ascii", 5, 5, "char");
+//     std::string input("jumpy fox");
+//     std::vector<std::string> label({"jumpy", "umpy ", "mpy f", "py fo", "y fox"});
+//     std::vector<std::string> out = tp.execute(input);
+//     CHECK(out == label);
+// }
+// TEST_CASE("TextPreprocessorTransformer_ngramscharwb") {
+//     std::vector<std::string> stopwords({});
+//     NS::Featurizers::Components::TextPreprocessorTransformer tp(true, "\\s+", stopwords, "ascii", 5, 5, "char_wb");
+//     std::string input("jumpy fox");
+//     std::vector<std::string> label({" jump", "jumpy", "umpy ", " fox "});
+//     std::vector<std::string> out = tp.execute(input);
+//     CHECK(out == label);
+// }
+// TEST_CASE("TextPreprocessorTransformer_lowercase_unicode") {
+//     std::vector<std::string> stopwords({});
+//     NS::Featurizers::Components::TextPreprocessorTransformer tp(true, "\\s+", stopwords, "unicode", 1, 1, "word");
+//     NS::Featurizers::Components::Utf8Converter converter(conv_error, wconv_error);
+// #if (defined __clang__)
+// #   pragma clang diagnostic push
+// #   pragma clang diagnostic ignored "-Wexit-time-destructors"
+// #endif
+//     std::wstring winput(L"HellO");
+//     std::string input = converter.to_bytes(winput);
+//     std::wstring prewlabel(L"hello");
+//     std::string prelabel = converter.to_bytes(prewlabel);
+//     std::vector<std::string> label({prelabel});
+// #if (defined __clang__)
+// #   pragma clang diagnostic pop
+// #endif
+//     std::vector<std::string> out = tp.execute(input);
+//     CHECK(out == label);
+// }
